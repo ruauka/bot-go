5988b07828e5d2550203bc3aac3469a1df1b2d38
ruauka <ruauka123@yandex.ru>
2022-11-09 20:28:44 +0300

    add deploy

--- bot/internal/domain/usecases/data.go
+++ bot/internal/domain/usecases/data.go
@@ -87,7 +87,7 @@ var (
 			tg.NewKeyboardButton("👦 Саша"),
 		),
 		tg.NewKeyboardButtonRow(
-			tg.NewKeyboardButton("👩 Маша4444"),
+			tg.NewKeyboardButton("👩 Маша"),
 		),
 		tg.NewKeyboardButtonRow(
 			tg.NewKeyboardButton("🌦 Погода"),
